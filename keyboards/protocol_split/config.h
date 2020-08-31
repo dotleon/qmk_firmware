35536b6788c6ec0eaaefd37b77b3177bcb1e616e
dotleon <dot@le.on>
2020-08-31 20:10:53 +0200

    fixed protocol seven via layout error on F8

--- keyboards/protocol_split/config.h
+++ keyboards/protocol_split/config.h
@@ -20,12 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x0000
+#define VENDOR_ID 0x1424
+#define PRODUCT_ID 0x0002
 #define DEVICE_VER 0x0001
 #define MANUFACTURER dotleon
-#define PRODUCT protokol_r
-#define DESCRIPTION A custom keyboard
+#define PRODUCT protocol_split
+#define DESCRIPTION protocol_split
 
 /* key matrix size */
 #define MATRIX_ROWS 12
